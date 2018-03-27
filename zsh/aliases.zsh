b97dd9449560c3a4316940092c0e10a146d42707
meinstein <max@datascience.com>
2018-03-27 16:03:04 -0700

    add alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,6 @@
 # Colorize output, add file type indicator, and put sizes in human readable format
 alias ll='ls -GAhl'
+alias ls='ls -GFh'
 
 alias platform='~/Desktop/ds/platform'
 alias darkroom='~/Desktop/ds/darkroom'
