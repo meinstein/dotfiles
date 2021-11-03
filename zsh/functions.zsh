416a8d848955c546bd4e0c72225b3a4878fd7fbe
Maximillian Einstein <maximillianeinstein@Maximillians-MacBook-Pro.local>
2021-11-03 12:46:27 +0100

    add nvm use function

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,5 +1,5 @@
 # Detect empty enter, execute git status if in git dir
-magic-enter () {
+magic-enter() {
         if [[ -z $BUFFER ]]; then
           if git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
             echo -ne '\n'
@@ -12,3 +12,15 @@ magic-enter () {
 }
 zle -N magic-enter
 bindkey "^M" magic-enter
+
+# run `nvm use` when cd'ing into a directory with a .nvmrc file.
+enter_directory() {
+  if [[ $PWD == $PREV_PWD ]]; then
+    return
+  fi
+
+  PREV_PWD=$PWD
+  [[ -f ".nvmrc" ]] && nvm use
+}
+
+chpwd_functions+=(enter_directory)
